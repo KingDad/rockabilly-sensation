3a6d7ca4be104c0d7ec5d2867ef795e05d728ef3
KingDad <pgrcarlson@gmail.com>
2017-12-11 13:40:22 -0500

    Working on a scroll activated animation. Restructured html

--- rockabillySensation/site/js/application.js
+++ rockabillySensation/site/js/application.js
@@ -1,4 +1,18 @@
 application = function(){
+  var controller = new ScrollMagic.Controller();
+  var doc = $(document);
+  var height = $(document).height();
+/*  var scrollage = document.body.scrollTop/document.body.;
+  var brenna = document.getElementById("brenna");
+  var brennaImg = document.getElementById("brenna-img");
+  function triggerAnimation(scroll){
+    scroll = scrollage;
+    if scroll ===
+  }*/
+  console.log(height);
+  console.log(8);
+  var scene = new ScrollMagic.Scene({
 
+  })
 }
 application();
