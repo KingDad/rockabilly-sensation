86851c7cefb4136da42598299a5257a6d93b68cf
KingDad <pgrcarlson@gmail.com>
2018-03-28 15:36:48 -0400

    Site style overhaul

--- rockabillySensation/site/js/application.js
+++ rockabillySensation/site/js/application.js
@@ -5,6 +5,8 @@ application = function(){
   var brenna = document.getElementById("brenna");
   var brennaImg = document.getElementById("brenna-img");
   var song = document.getElementById("song");
+  var x = 0;
+  //trigger animations based on scroll placement using CSS keyframe animations
   function triggerAnimation(scroll){
     scroll = scrollage;
     console.log("Scrolled");
@@ -13,8 +15,23 @@ application = function(){
       $(song).addClass("lightened");
     }
   }
-  triggerAnimation();
+  //first attempt at ScrollMagic
+  var controller = new ScrollMagic.Controller();
+  var scene = new ScrollMagic.Scene({
+    offset: 0,
+    //you are still abstracting the math to make this last the appropriate amount of time
+    duration: "30%",
+    reverse: true
+  });
+  //disabled core function for now, trying to see if I can get something to work
+//  triggerAnimation();
+
+//uhhh not sure what to do with this while loop but I'll crush it tonight, right me? Thanks, me
+while (x = 0){
+  brenna.style.height = scrollage + "%";
+};
   console.log(height);
   console.log(scrollage);
+
 }
 application();
